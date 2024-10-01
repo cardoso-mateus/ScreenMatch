2fe1c33ae495ada1a74bf9a48ac902df49a119b9
Mateus Cardoso <cardoso.mateusv@gmail.com>
2024-10-01 17:51:22 -0300

    Update DadosEpisodio.java

--- src/main/java/br/com/alura/screenmatch/model/DadosEpisodio.java
+++ src/main/java/br/com/alura/screenmatch/model/DadosEpisodio.java
@@ -1,7 +1,9 @@
 package br.com.alura.screenmatch.model;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record DadosEpisodio(
         @JsonAlias() String titulo,
         @JsonAlias() Integer numeroEpisodio,
