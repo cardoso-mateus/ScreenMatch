b772a9a3aca82ba5e8339adcbfd2abb26811b090
Mateus Cardoso <cardoso.mateusv@gmail.com>
2024-10-01 17:50:52 -0300

    Update DadosTemporada.java

--- src/main/java/br/com/alura/screenmatch/model/DadosTemporada.java
+++ src/main/java/br/com/alura/screenmatch/model/DadosTemporada.java
@@ -3,6 +3,8 @@ package br.com.alura.screenmatch.model;
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import java.util.List;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record DadosTemporada(
     @JsonAlias() Integer numeroTemporada,
