5cac70dd6a5c087abcbaf897580d88d4beeb7704
Mateus Cardoso <cardoso.mateusv@gmail.com>
2024-10-02 20:40:25 -0300

    Update ScreenmatchApplication.java

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -21,17 +21,17 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		ConverteDados conversor = new ConverteDados();
 
 		var json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
-		System.out.println(json);
+		System.out.println(json + "\n");
 
 		DadosSerie dadosSerie = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dadosSerie);
+		System.out.println(dadosSerie + "\n");
 
 		for (int i = 1; i <= dadosSerie.totalTemporadas(); i++) {
 			json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&Season=" + i + "&apikey=6585022c");
-			System.out.println(json);
+			System.out.println(json + "\n");
 
 			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-			System.out.println(dadosTemporada);
+			System.out.println(dadosTemporada + "\n");
 		}
 	}
 }
