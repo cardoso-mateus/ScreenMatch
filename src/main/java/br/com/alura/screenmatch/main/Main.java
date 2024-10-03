2fc26ed3136d4fc6013762fc1eb4cf1f8ce7e722
Mateus Cardoso <cardoso.mateusv@gmail.com>
2024-10-02 22:23:47 -0300

    Update Main.java

--- src/main/java/br/com/alura/screenmatch/main/Main.java
+++ src/main/java/br/com/alura/screenmatch/main/Main.java
@@ -1,18 +1,39 @@
 package br.com.alura.screenmatch.main;
 
+import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.model.DadosTemporada;
 import br.com.alura.screenmatch.service.ConsumoAPI;
+import br.com.alura.screenmatch.service.ConverteDados;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    private Scanner leitura = new Scanner(System.in);
-    private ConsumoAPI consumo = new ConsumoAPI();
+    private Scanner input = new Scanner(System.in);
+    private ConsumoAPI consumoAPI = new ConsumoAPI();
+    private ConverteDados converteDados = new ConverteDados();
     private final String ENDERECO = "https://www.omdbapi.com/?t=";
     private final String API_KEY = "&apikey=6585022c";
     public void exibeMenu() {
         System.out.println("Digite a série desejada.");
-        var nomeSerie = leitura.nextLine();
-        var URI = ENDERECO + nomeSerie.replace(" ", "+") + API_KEY;
-        consumo.obterDados(URI);
+        var nomeSerie = input.nextLine();
+
+        var url = ENDERECO + nomeSerie.replace(" ", "+") + API_KEY;
+        var jsonRequest = consumoAPI.getRequestData(url);
+
+        DadosSerie dadosSerie = converteDados.parseData(jsonRequest, DadosSerie.class);
+
+        List<DadosTemporada> temporadas = new ArrayList<>();
+
+        for (int i = 1; i <= dadosSerie.totalTemporadas(); i++) {
+            url = ENDERECO + nomeSerie.replace(" ", "+") + "&Season=" + i + API_KEY;
+            jsonRequest = consumoAPI.getRequestData(url);
+            DadosTemporada dadosTemporada = converteDados.parseData(jsonRequest, DadosTemporada.class);
+            temporadas.add(dadosTemporada);
+        }
+
+        System.out.println("\n" + dadosSerie + "\n");
+        temporadas.forEach(System.out::println);
     }
 }
